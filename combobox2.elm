3b6acf84ffc4001a88f3bc5146d33efc2ee0a341
brian buckley <briankbuckley@gmail.com>
2017-10-25 21:54:59 -0400

    add combobox2

---
+++ combobox2.elm
@@ -0,0 +1,132 @@
+module Combobox2 exposing (..)
+
+import Html exposing (Html)
+import Html.Attributes as Attributes
+import Kintail.InputWidget as K exposing (checkbox)
+
+
+type FooBar
+    = Foo
+    | Bar
+    | Nil
+
+
+type Suit
+    = Hearts
+    | Spades
+    | Diamonds
+    | Clubs
+
+
+type Title
+    = Dr
+    | Mrs
+    | Ms
+    | Mr
+    | Master
+    | Miss
+
+
+type alias Model =
+    { title : Title
+    , firstName : String
+    , lastName : String
+    , foobar : FooBar
+    , suit : Suit
+    , msuit : Maybe Suit
+    }
+
+
+type Msg
+    = NewTitle Title
+    | NewFirstName String
+    | NewLastName String
+    | NewFooBar FooBar
+    | NewSuit Suit
+    | NewMSuit (Maybe Suit)
+
+
+update : Msg -> Model -> Model
+update message model =
+    case message of
+        NewTitle newTitle ->
+            { model | title = newTitle }
+
+        NewFirstName newFirstName ->
+            { model | firstName = newFirstName }
+
+        NewLastName newLastName ->
+            { model | lastName = newLastName }
+
+        NewFooBar foobar ->
+            { model | foobar = foobar }
+
+        NewSuit suit ->
+            { model | suit = suit }
+
+        NewMSuit msuit ->
+            { model | msuit = msuit }
+
+
+toStringFooBar : FooBar -> String
+toStringFooBar foobar =
+    case foobar of
+        Foo ->
+            "Foo"
+
+        Bar ->
+            "Bar"
+
+        Nil ->
+            ""
+
+
+toStringMaybe : Maybe a -> String
+toStringMaybe a =
+    case a of
+        Just v ->
+            toString v
+
+        Nothing ->
+            ""
+
+
+viewSuit : Suit -> Model -> Html Msg
+viewSuit suit model =
+    Html.span []
+        [ Html.label [ Attributes.for (toString suit) ] [ Html.text (toString suit) ]
+        , K.radioButton [] suit model.suit |> Html.map NewSuit
+        ]
+
+
+viewSuits : Model -> Html Msg
+viewSuits model =
+    Html.div [] (List.map (\x -> viewSuit x model) [ Hearts, Spades, Diamonds, Clubs ])
+
+
+view : Model -> Html Msg
+view model =
+    Html.div []
+        [ K.comboBox [] toString [ Dr, Mrs, Ms, Mr, Master, Miss ] model.title
+            |> Html.map NewTitle
+        , K.lineEdit [] model.firstName
+            |> Html.map NewFirstName
+        , K.lineEdit [] model.lastName
+            |> Html.map NewLastName
+        , K.comboBox [] toString [ Nil, Foo, Bar ] model.foobar |> Html.map NewFooBar
+        , K.comboBox [] toStringFooBar [ Nil, Foo, Bar ] model.foobar |> Html.map NewFooBar
+        , K.comboBox [] toString [ Hearts, Spades, Diamonds, Clubs ] model.suit |> Html.map NewSuit
+        , K.comboBox [] toStringMaybe [ Nothing, Just Hearts, Just Spades, Just Diamonds, Just Clubs ] model.msuit |> Html.map NewMSuit
+        , viewSuits model
+        , Html.br [] []
+        , Html.text (toString model)
+        ]
+
+
+main : Program Never Model Msg
+main =
+    Html.beginnerProgram
+        { model = Model Dr "Albert" "Einstein" Foo Clubs Nothing
+        , update = update
+        , view = view
+        }
