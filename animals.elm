176f3c02982e56637d12b76cd3536bfc525bea6a
brian buckley <briankbuckley@gmail.com>
2017-11-06 09:45:45 -0500

    tweak animal.elm

--- animals.elm
+++ animals.elm
@@ -25,34 +25,48 @@ type Msg
     = SelectMessage (Maybe Animal)
 
 
+model : Model
 model =
-    { animals = [ Animal "0" "Bär", Animal "1" "Dolphin", Animal "3" "Bee" ]
+    { animals = [ Animal "0" "Bär", Animal "1" "Dolphin", Animal "3" "Bee", Animal "4" "Ant" ]
     , selected = Nothing
     }
+
+
+x : String -> Msg
+x =
+    SelectMessage << getAnimalFromId model.animals
 
 
 view : Model -> Html Msg
 view model =
     div []
         [ select
-            [ onInput (SelectMessage << getAnimalFromId model.animals) ]
+            [ onInput x ]
+            --    [ onInput (SelectMessage << getAnimalFromId model.animals) ]
             (List.map viewOptions model.animals)
         , Maybe.map .name model.selected
             |> Maybe.withDefault "--"
             |> text
         , p [] [ text (toString model) ]
-        , p [] [ text (toString (SelectMessage << getAnimalFromId model.animals)) ]
+        , p [] [ text (toString x) ]
         ]
 
 
+xx : Animal -> Html.Attribute msg
+xx animal =
+    value <| toString animal.id
+
+
+viewOptions : Animal -> Html msg
 viewOptions animal =
-    option [ value <| toString animal.id ] [ text animal.name ]
+    --  option [ value <| toString animal.id ] [ text animal.name ]
+    option [ xx animal ] [ text animal.name ]
 
 
 getAnimalFromId : List Animal -> String -> Maybe Animal
 getAnimalFromId animals id =
-    List.filter (\a -> toString a.id == id) animals
-        |> List.head
+    -- List.filter (\a -> toString a.id == id) animals |> List.head
+    List.filter (\a -> a.id == id) animals |> List.head
 
 
 update msg model =
