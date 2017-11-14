103712287fc88b3449310eb04d0d317a2bc05a05
brian buckley <briankbuckley@gmail.com>
2017-11-14 12:53:46 -0500

    decode.elm

--- decode.elm
+++ decode.elm
@@ -1,12 +1,12 @@
 module Main exposing (..)
 
 import Html exposing (..)
-import Json.Decode exposing (bool, decodeString, field, int, list)
+import Json.Decode exposing (bool, decodeString, field, int, list, string)
 import Json.Decode.Pipeline exposing (decode, hardcoded, optional, required)
 
 
 main =
-    xxxxx |> toString |> text
+    xxxxxxx |> toString |> text
 
 
 a =
@@ -54,9 +54,52 @@ type alias Point =
 
 pointDecoder : Json.Decode.Decoder Point
 pointDecoder =
-    decode Point |> required "x" int |> required "y" int
+    decode Point
+        |> required "x" int
+        |> required "y" int
 
 
 xxxxx : Result String Point
 xxxxx =
     decodeString pointDecoder """{ "x": 3, "y": 4 }"""
+
+
+type alias Tc =
+    { id : String
+    , dob : String
+    , doe : String
+    , crd : String
+    , stat : String
+    , ric : String
+    }
+
+
+tcDecoder : Json.Decode.Decoder Tc
+tcDecoder =
+    decode Tc
+        |> required "id" string
+        |> required "dob" string
+        |> required "doe" string
+        |> required "crd" string
+        |> required "stat" string
+        |> required "ric" string
+
+
+tc1 =
+    """{ "id": "123", "dob": "","doe": "","crd": "11","stat": "11","ric": "11" }"""
+
+
+tc2 =
+    """{ "id": "124", "dob": "","doe": "","crd": "11","stat": "11","ric": "11" }"""
+
+
+tc3 =
+    """{ "id": "125", "dob": "","doe": "","crd": "11","stat": "11","ric": "11" }"""
+
+
+xxxxxx =
+    decodeString tcDecoder """{ "id": "123", "dob": "","doe": "","crd": "11","stat": "11","ric": "11" }"""
+
+
+xxxxxxx =
+    decodeString (list tcDecoder) """[{ "id": "123", "dob": "","doe": "","crd": "11","stat": "11","ric": "11" },{ "id": "123", "dob": "","doe": "","crd": "11","stat": "11","ric": "11" }]"""
