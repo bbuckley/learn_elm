f9bd91e104fab69b623ed0336e8c5d59a3caf478
brian buckley <briankbuckley@gmail.com>
2017-11-25 20:34:31 -0500

    pointfreemelm

--- pointfree.elm
+++ pointfree.elm
@@ -17,7 +17,8 @@ main =
         |> isEven
         |> List.map (\x -> x + 10)
         |> List.map (\x -> x * 10)
-        |> List.filter (\x -> x % 40 /= 0)
+        -- |> List.filter (\x -> x % 40 /= 0)
+        |> List.map (\x -> ( x, True ))
         |> toString
         |> text
 
