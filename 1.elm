9b4adff98a7a9457045b9eb1ee2d0d49cc8747b5
Brian Buckley <briankbuckley@gmail.com>
2019-07-28 13:32:38 -0400

    tweak 1.elm

--- 1.elm
+++ 1.elm
@@ -5,9 +5,12 @@ import Html exposing (Html, text)
 
 xxx : Int
 xxx =
-    11
+    -- 11
+    --List.foldl (\_ a -> 1 + a) 0 (List.repeat 3 11)
+    List.repeat 4 "" |> List.foldl (\_ a -> 1 + a) 0
 
 
 main : Html msg
 main =
-    text ("test " ++ String.fromInt xxx)
+    -- text ("test " ++ String.fromInt xxx)
+    "tester " ++ String.fromInt xxx |> text
