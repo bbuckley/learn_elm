50de815275c1e4112c77062a81bcbf0f5b8542f5
Brian Buckley <briankbuckley@gmail.com>
2019-09-04 08:22:35 -0400

    1.elm changes

--- 1.elm
+++ 1.elm
@@ -3,14 +3,11 @@ module Main exposing (main)
 import Html exposing (Html, text)
 
 
-xxx : Int
-xxx =
-    -- 11
-    --List.foldl (\_ a -> 1 + a) 0 (List.repeat 3 11)
-    List.repeat 4 "" |> List.foldl (\_ a -> 1 + a) 0
+x : List a -> Int
+x =
+    List.foldl (\_ a -> 1 + a) 0
 
 
 main : Html msg
 main =
-    -- text ("test " ++ String.fromInt xxx)
-    "tester " ++ String.fromInt xxx |> text
+    "tester " ++ String.fromInt (x [ 1, 7, 8 ]) |> text
